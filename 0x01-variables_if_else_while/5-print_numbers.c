c21a5b7199c3c139bf56e97d19c060ef6205f094
Your Name <you@example.com>
2023-02-19 07:26:47 -0800

    Print Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,15 @@
  *
  * Return: 0
  */
+
 int main(void)
 {
 	int num = 0;
 
-	while (num <= 10)
-		 printf("%d \n", num++);
+	while (num < 10)
+		 printf("%d", num++);
+
+	printf('\n')
+	
 	return (0);
 }
