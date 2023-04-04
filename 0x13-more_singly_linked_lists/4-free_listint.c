9d30d8012a2006aacdd6307a5010008a19510b87
Your Name <you@example.com>
2023-04-04 12:11:01 -0700

    Length of Linked Lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - Free a listint_t list.
+ * @head: Pointer to the head of the listint_t list to be freed.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
