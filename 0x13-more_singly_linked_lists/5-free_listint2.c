7444ce02ae42daccade7f1b3faca6a27517f2f82
Your Name <you@example.com>
2023-04-04 12:39:07 -0700

    Tasks 5-7

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: A pointer to the address of the head of the listint_t list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node = *head;
+	listint_t *next;
+
+	while (current_node != NULL) 
+	{
+		next = current->next;
+
+		free(current_node);
+
+		current_node = next;
+	}
+
+	head = NULL;
+}
+
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes head node of a listint_t list.
+ * @head: Pointer to a pointer that hold address of the head of the listint_t list.
+ * Return: If the linked list is empty - 0, but if not empty, the head node data - n
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int node_int_value;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	node_int_value = head->n;
+	*head = head->next;
+
+	free(tmp);
+
+	return (node_int_value);
+}
+
